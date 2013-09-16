8ff8bbe2dbb5f6c85099ba74ecc4af27d5d36fdc
Ryan Olson <ryanolsonk@gmail.com>
2013-09-16 00:36:39 -0700

    Add basic quick look data providers

---
+++ DataProviders/NSObject+LLDBQuickLook.h
@@ -0,0 +1,44 @@
+//
+//  NSObject+LLDBQuickLook.h
+//  SlyBits
+//
+//  Created by Ryan Olson on 9/15/13.
+//
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2013 Ryan Olson
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of
+//  this software and associated documentation files (the "Software"), to deal in
+//  the Software without restriction, including without limitation the rights to
+//  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+//  the Software, and to permit persons to whom the Software is furnished to do so,
+//  subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#import <Foundation/Foundation.h>
+
+// An informal protocol that objects can choose to implement if they will benefit from Quick Look debugging.
+// Default implementations are provided on NSObject to keep the debugger from barfing when it calls the selectors.
+@interface NSObject (LLDBQuickLook)
+
+// The data returned by this method will be saved to a tmp file by the lldb commands 'ql' and 'qlf'.
+// The tmp file will then be opened with Quick Look.
+- (NSData *)debugData;
+
+// The filename returned by this method will be used for the tmp file.
+// Specifying a propper extension for the file helps Quick Look display the file
+// and choose an appropriate application to open the file.
+- (NSString *)debugFilename;
+
+@end
---
+++ DataProviders/NSObject+LLDBQuickLook.m
@@ -0,0 +1,51 @@
+//
+//  NSObject+LLDBQuickLook.m
+//  SlyBits
+//
+//  Created by Ryan Olson on 9/15/13.
+//
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2013 Ryan Olson
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of
+//  this software and associated documentation files (the "Software"), to deal in
+//  the Software without restriction, including without limitation the rights to
+//  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+//  the Software, and to permit persons to whom the Software is furnished to do so,
+//  subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#import "NSObject+LLDBQuickLook.h"
+
+@implementation NSObject (LLDBQuickLook)
+
+// A default implementation so the debugger doesn't barf when calling the selector.
+- (NSData *)debugData
+{
+    return nil;
+}
+
+// The default filename consists of the class name of the object and a string of digits from the timestamp
+// There is no extension, as we don't have a good way to know what kind of file we're saving.
+// NSObject subclasses can use this base implementation, but should at least append an appropriate file extension.
+- (NSString *)debugFilename
+{
+    // By putting this into an NSUInteger, we might loose the most significant digits.
+    // That's ok, we're really just care about a reasonably unique string for the filename.
+    NSUInteger timestampInMilliseconds = (NSUInteger)([NSDate timeIntervalSinceReferenceDate] * 1000);
+    NSString *className = NSStringFromClass([self class]);
+    return [NSString stringWithFormat:@"%@_%u", className, timestampInMilliseconds];
+}
+
+@end
