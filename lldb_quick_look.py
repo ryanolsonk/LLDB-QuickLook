0953c8fbc217c368c23e8c6c1ad831b3a7d550d2
Ryan Olson <ryanolsonk@gmail.com>
2013-09-17 23:46:59 -0700

    Consolidate the two lldb commands into one command with options.
    The new -f option allows choosing the filename straight from the command.

--- lldb_quick_look.py
+++ lldb_quick_look.py
@@ -23,44 +23,92 @@
 import lldb
 import os
 import subprocess
+import optparse
+import shlex
 
 
 class DebuggerError(Exception):
-    """Exception raised when when a debugger command cannot proceed due to bad state or invalid input"""
+    """
+    Exception raised when when a debugger command cannot proceed due to bad state or invalid input
+    """
     pass
+
+
+def create_quick_look_options():
+    usage = "usage: %prog <options> -- <expr>"
+    parser = optparse.OptionParser(prog='quicklook', usage=usage)
+    parser.add_option('-f', '--filename', type='string', dest='filename', help='Override filename for saving the data', metavar='filename.ext')
+    parser.add_option('-l', '--lite', action='store_true', dest='lite', help='Use the stripped down version of Quick Look (qlmanage)', default=False)
+    return parser
+
 
 def quick_look_command(debugger, command, result, internal_dict):
     """
-    Quick Look Full (qlf): A command to save arbitrary data from a debugging session 
-    to a tmp location & open in Finder with the full version of Quick Look
-    Invoke as
-    (lldb) qlf <object>
-    e.g.
-    (lldb) qlf self.view
+    Overview:
+
+    A command to inspect objects that are not well represented by strings in the debug console.
+    The command will ask an object for its quickLookDebugData and save that data to the object's 
+    quickLookFilename or a custom filename passed using the -f option. The file will be saved under /tmp/<target>
+    You can make any object into a quick look data provider by simply implementing the quickLookDebugData and quickLookFilename methods.
+
+    Examples:
+
+    quicklook self.imageView.image
+    quicklook [[UIApplication sharedApplication] keyWindow]
+
+    Some use cases:
+
+    Debugging images not accessible from the Xcode GUI.
+    Debugging views that are not on screen or obscured by another view.
+    Saving images for screenshots or to watch a view change over time.
+    Opening text in a propper editor without having to copy/paste.
+
+    IMPORTANT NOTE:
+
+    Because this command takes 'raw' input, if you use any
+    command options you must use ' -- ' between the end of the command options
+    and the beginning of the raw input.
+
+
     """
+
+    # Separate the options from the expression
+    command_list = shlex.split(command)
     try:
-        savedFilePath = get_data_and_save(debugger, command)
+        # "--" indicates the end of the arguments and beginning of the expression
+        dashdash_idx = command_list.index("--")
+        arguments = command_list[:dashdash_idx]
+        # strip everything up to and including the "-- " to get the expression
+        expression = command[(command.rindex("--") + 3):]
+    except ValueError:
+        # no "--" found, assume no arguments
+        arguments = []
+        expression = command
+
+
+    # Parse the options
+    parser = create_quick_look_options()
+    try:
+        (options, args) = parser.parse_args(arguments)
+    except:
+        result.SetError("Option parsing failed")
+        return
+
+
+    # Save the data
+    try:
+        savedFilePath = get_data_and_save(debugger, expression, options.filename)
         print >> result, "Data written to {0!s}".format(savedFilePath)
-        open_with_quick_look(savedFilePath)
     except DebuggerError, err:
-        print >> result, "Error: {0!s}".format(err)
+        result.SetError(str(err))
+        return
 
 
-def quick_look_lite_command(debugger, command, result, internal_dict):
-    """
-    Quick Look (ql): A command to save arbitrary data from a debugging session to a tmp location 
-    and open using qlmanage - a stripped down version of Quick Look invokable from the command line.
-    Invoke as
-    (lldb) ql <object>
-    e.g.
-    (lldb) ql self.imageView.image
-    """
-    try:
-        savedFilePath = get_data_and_save(debugger, command)
-        print >> result, "Data written to {0!s}".format(savedFilePath)
+    # Open with Quick Look
+    if options.lite:
         open_with_quick_look_lite(savedFilePath)
-    except DebuggerError, err:
-        print >> result, "Error: {0!s}".format(err)
+    else:
+        open_with_quick_look(savedFilePath)
 
 
 def open_with_quick_look(filePath):
@@ -99,9 +147,9 @@ def open_with_quick_look_lite(filePath):
     subprocess.Popen(["qlmanage", "-p", filePath])
 
 
-def get_data_and_save(debugger, command):
+def get_data_and_save(debugger, expression, overrideFilename):
     """
-    Asks the object passed in command for its debugData and debugFilename.
+    Asks the object passed in expression for its debugData and debugFilename.
     Saves the data to the filename under /tmp/{target}
     Raises DebuggerError exceptions for bad state or invalid commands
     Returns the path of the saved file in the success case.
@@ -112,24 +160,19 @@ def get_data_and_save(debugger, command):
     frame = thread.GetSelectedFrame()
 
     if not target.IsValid() or not process.IsValid():
-        raise DebuggerError("unable to get target/process")
+        raise DebuggerError("Unable to get target/process")
 
     options = lldb.SBExpressionOptions()
     options.SetIgnoreBreakpoints()
 
-    dataCommand = "(NSData *)[({0!s}) quickLookDebugData]".format(command)
-    filenameCommand = "(NSString *)[({0!s}) quickLookDebugFilename]".format(command)
-
-    
+    dataCommand = "(NSData *)[({0!s}) quickLookDebugData]".format(expression)
     if frame.IsValid():
         data = frame.EvaluateExpression(dataCommand, options)
-        filename = frame.EvaluateExpression(filenameCommand, options).GetObjectDescription()
     else:
         data = target.EvaluateExpression(dataCommand, options)
-        filename = target.EvaluateExpression(filenameCommand, options).GetObjectDescription()
 
     if data.GetValueAsUnsigned() == 0:
-        raise DebuggerError("can't get debug data. Make sure the object has a non-nil response to the selector debugData")
+        raise DebuggerError("Can't get debug data from {0!s}. Make sure the object has a non-nil response to the selector quickLookDebugData".format(expression))
 
     lengthCommand = "(NSUInteger)[({0!s}) length]".format(data.path)
     length = target.EvaluateExpression(lengthCommand, options).GetValueAsUnsigned()
@@ -140,11 +183,24 @@ def get_data_and_save(debugger, command):
     error = lldb.SBError()
     memoryBuffer = process.ReadMemory(bytes, length, error)
     if error.Fail():
-        raise DebuggerError("couldn't read memory: {0!s}".format(error))
+        raise DebuggerError("Couldn't read memory: {0!s}".format(error))
 
     directory = os.path.join("/tmp", str(target))
     if not os.path.exists(directory):
         os.makedirs(directory)
+
+    # If a filename was not specfied with the command, ask the expression for it
+    if overrideFilename is None:
+        filenameCommand = "(NSString *)[({0!s}) quickLookDebugFilename]".format(expression)
+        if frame.IsValid():
+            filename = frame.EvaluateExpression(filenameCommand, options).GetObjectDescription()
+        else:
+            filename = target.EvaluateExpression(filenameCommand, options).GetObjectDescription()
+    else:
+        filename = overrideFilename
+
+    if filename is None:
+        raise DebuggerError("No filename provided. Please specify one in quickLookDebugFilename or by passing a name in the -f option")
 
     path = os.path.join(directory, str(filename))
     with open(path, "w") as dataFile:
@@ -154,6 +210,7 @@ def get_data_and_save(debugger, command):
 
 
 def __lldb_init_module(debugger, internal_dict):
-    debugger.HandleCommand('command script add --function lldb_quick_look.quick_look_command qlf')
-    debugger.HandleCommand('command script add --function lldb_quick_look.quick_look_lite_command ql')
+    parser = create_quick_look_options()
+    quick_look_command.__doc__ = quick_look_command.__doc__ + parser.format_help()
+    debugger.HandleCommand('command script add --function lldb_quick_look.quick_look_command quicklook')
 
